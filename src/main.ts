32fd958db8a1c864c53f4f6846e9aa1573110cf8
Hazalcan <hazalcanelersu@gmail.com>
2025-02-24 21:36:22 +0300

    port number config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3001);
+
+  console.log(`Application is running port: ${process.env.PORT || 3001}`);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
