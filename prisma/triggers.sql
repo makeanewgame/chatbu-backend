978612397bfada7a0e877035077cc667f38eccc2
Hazalcan <hazalcanelersu@gmail.com>
2025-04-19 12:01:05 +0300

    storage file updated

--- prisma/triggers.sql
+++ prisma/triggers.sql
@@ -3,17 +3,15 @@ SET search_path TO backend;
 CREATE OR REPLACE FUNCTION notify_storage_update()
 RETURNS trigger AS $$
 BEGIN
-PERFORM pg_notify('storage_updates', row_to_json(NEW)::text);
-RETURN NEW;
+  PERFORM pg_notify('storage_updates', row_to_json(NEW)::text);
+  RETURN NEW;
 END;
-
-$$
-LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
 DROP TRIGGER IF EXISTS storage_update_trigger ON "Storage";
+
 CREATE TRIGGER storage_update_trigger
 AFTER UPDATE ON "Storage"
 FOR EACH ROW
 WHEN (OLD IS DISTINCT FROM NEW)
 EXECUTE FUNCTION notify_storage_update();
-$$
