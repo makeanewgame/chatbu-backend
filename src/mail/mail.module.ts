2f80a36ed643e1c08caf372cc96dbbca0aed0046
Hazalcan <hazalcanelersu@gmail.com>
2026-03-29 17:58:48 +0300

    chore: update prisma dependencies to version 7.6.0 in package.json
    fix(mail): update HandlebarsAdapter import path in mail.module.ts

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/adapters/handlebars.adapter';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { join } from 'path';
 
