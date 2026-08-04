bcff68b3a6495bc63d1aaea4bb954c8eee122d0b
Hazalcan <hazalcanelersu@gmail.com>
2026-08-04 12:08:38 +0300

    updated

--- src/authentication/dto/lost.request.ts
+++ src/authentication/dto/lost.request.ts
@@ -4,11 +4,4 @@ export class LostRequest {
   @IsString()
   @IsNotEmpty()
   email: string;
-  @IsString()
-  @IsNotEmpty()
-  code: string;
-  updated_at: string;
-  created_at: string;
-  refreshtoken: string;
-  emailVerified: boolean;
 }
